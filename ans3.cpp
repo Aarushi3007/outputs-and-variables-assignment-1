ac4b5a1b5046bcd4b51130373ddebfe61b7ef035
Arushi mishra <129285923+Aarushi3007@users.noreply.github.com>
2023-10-14 10:17:39 +0530

    solved assignment files are added

---
+++ ans3.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"size  of  int  is "<<sizeof(int)<<endl;
+    cout<<"size  of  int  is "<<sizeof(bool)<<endl;
+    cout<<"size  of  int  is "<<sizeof(float)<<endl;
+}
---
+++ ans5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int  r,circumference;
+    cout<<"enter the radius ";
+    cin>>r;
+    circumference = 2*3.14*r;
+    cout<<"circumference is "<<circumference;
+}
